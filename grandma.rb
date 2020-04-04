b9fbf1dfebbf38f3f48c36bc8552b1f4acdea5cf
Evan Burton <evan@heyevan.com>
2020-04-04 00:27:50 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -5,8 +5,11 @@ def speak_to_grandma(string)
     return "HUH?! SPEAK UP, SONNY!"
   elsif string.upcase
     return "NO, NOT SINCE 1938!"
+  else
+    if string == "I LOVE YOU GRANDMA"
+    return "I LOVE YOU, TOO PUMPKIN!"
   end
-end 
+end
 
 # Write a speak_to_grandma method.
 
