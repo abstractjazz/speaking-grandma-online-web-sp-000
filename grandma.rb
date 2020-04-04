4b238686c8ae63efaf80cbeb4c72158bb0e0633f
Evan Burton <evan@heyevan.com>
2020-04-04 00:04:24 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -3,12 +3,12 @@ require 'pry'
 
 
 def speak_to_grandma(string)
-  if string = string.upcase
+  if string = "I LOVE YOU GRANDMA!"
  return "I LOVE YOU TOO PUMPKIN!"
-elsif string = string.downcase
- return "HUH?! SPEAK UP, SONNY!"
+elsif string = string.upcase
+ return "NO, NOT SINCE 1938!"
 else
-   return "NO, NOT SINCE 1938!"
+"HUH?! SPEAK UP SONNY!"
   end
 end
 
